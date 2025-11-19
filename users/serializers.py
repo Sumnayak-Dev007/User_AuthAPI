32e024b6cd6c93fd4c8381ccbf2f840aaa0716d2
Sumnayak-Dev007 <nsumankumari225@gmail.com>
2025-11-19 16:02:40 +0530

    debugging

--- users/serializers.py
+++ users/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import CustomUser
 from django.contrib.auth.password_validation import validate_password
+from django.core.exceptions import ValidationError as DjangoValidationError
+
 
 class RegisterSerializer(serializers.ModelSerializer):
     username = serializers.CharField(
